9efd230c69c9cf40c77659adc14dcf71ce52cdd9
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 <tusharpatil22700@gmail.com>
2022-10-13 15:01:47 -0700

    0Position

--- src/com/bridgeLabz/SnakeAndLadder.java
+++ src/com/bridgeLabz/SnakeAndLadder.java
@@ -3,7 +3,10 @@ package com.bridgeLabz;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
+		 int pos = 0;
 		System.out.println("Welcome to Snake and ladder Program");
+        System.out.println("Single player at start position "+ pos);
+
 	}
 
 }
