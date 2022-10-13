ea3c74f03d3f683afe3edefe6b011f02fc00b0e8
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 <tusharpatil22700@gmail.com>
2022-10-13 15:34:24 -0700

    The player roll

--- src/com/bridgeLabz/SnakeAndLadder.java
+++ src/com/bridgeLabz/SnakeAndLadder.java
@@ -1,11 +1,21 @@
 package com.bridgeLabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		 int pos = 0;
+		 int pos = 0;	 
 		System.out.println("Welcome to Snake and ladder Program");
         System.out.println("Single player at start position "+ pos);
+       RollDisk();
+	}
+
+	 public static void RollDisk() {
+		 Random random = new Random();
+	        int dice = random.nextInt(6);
+	        dice = dice+1;
+	        System.out.println("Dice Num : "+ dice);
 
 	}
 
